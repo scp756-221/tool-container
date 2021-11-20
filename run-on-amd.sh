95ee0f226b08cd01184adf8add3a6be121d023af
Ted Kirkpatrick <ted@sfu.ca>
2021-11-19 17:00:22 -0800

    Pass REGID and INAME to shell.sh

--- run-on-amd.sh
+++ run-on-amd.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o nounset
 set -o errexit
+regid=$(cat ${VER_HOME}/regid.txt)
+iname=$(cat ${VER_HOME}/iname.txt)
 ver=$(cat ${VER_HOME}/version.txt)
 set -o xtrace
-tools/shell.sh ${ver}-amd64
+REGID=${regid} INAME=${iname} tools/shell.sh ${ver}-amd64
--- run-on-arm.sh
+++ run-on-arm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -o nounset
 set -o errexit
+regid=$(cat ~/regid.txt)
+iname=$(cat ~/iname.txt)
 ver=$(cat ~/version.txt)
-sudo HOME=/home/ubuntu tools/shell.sh ${ver}-arm64
+sudo HOME=/home/ubuntu REGID=${regid} INAME=${iname} tools/shell.sh ${ver}-arm64
