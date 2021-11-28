f6b8855fc04ff4c6550f87591aff33be3ddede28
Ted Kirkpatrick <ted@sfu.ca>
2021-11-27 16:38:10 -0800

    Add optional userid to scripts for remote instances
    EC2 instances running Ubuntu require signing on as 'ubuntu',
    while instances running Amazon Linux require signing on as 'ec2-user'.
    
    This revision adds an optional userid to `signon.sh`.  The script
    saves that userid to be used by `transfer.sh` whenever a file is
    to be sent to the current remote instance.

--- signon.sh
+++ signon.sh
@@ -2,15 +2,26 @@
 # Sign on to EC2 instance via ssh
 set -o errexit
 set -o nounset
-if [[ $# != 1 ]]
+default_userid='ubuntu'
+if [[ $# -lt 1 || $# -gt 2 ]]
 then
-  echo "Usage: signon.sh EC2-INSTANCE-NAME"
+  echo "Usage: signon.sh EC2-INSTANCE-NAME [EC2-USERID]"
   echo " This command will save the instance name for later use by transfer.sh."
   echo " EC2-INSTANCE-NAME should not include the userid for signing on."
-  echo " The instance userid must be 'ubuntu'."
+  echo " The instance userid is optional, with default '${default_userid}'."
+  echo " Amazon Linux requires the userid 'ec2-user'."
   exit 1
 fi
+
+if [[ $# == 1 ]]
+then
+  userid=${default_userid}
+else
+  userid=${2}
+fi
+
 echo ${1} > ec2-instance-name.txt
+echo ${userid} > ec2-userid.txt
 pem=$(cat ec2-pem-path.txt)
 set -o xtrace
-ssh -i ${pem} ubuntu@${1}
+ssh -i ${pem} ${userid}@${1}
--- transfer.sh
+++ transfer.sh
@@ -2,8 +2,23 @@
 # Transfer file to ARM EC2 instance
 set -o errexit
 set -o nounset
-base=$(basename ${1})
+if [[ $# -lt 1 || $# -gt 2 ]]
+then
+  echo "Usage: transfer.sh PATH [REMOTE-FILE-NAME]"
+  echo " This command will send the file at PATH to the home directory of"
+  echo " the machine and userid named in the last call to signon.sh."
+  echo " If a second argument is provided, the remote file will have that name."
+  exit 1
+fi
+
+if [[ $# == 1 ]]
+then
+  base=$(basename ${1})
+else
+  base=${2}
+fi
 pem=$(cat ec2-pem-path.txt)
+userid=$(cat ec2-userid.txt)
 ec2_instance=$(cat ec2-instance-name.txt)
 set -o xtrace
-scp -i ${pem} ${1} ubuntu@${ec2_instance}:/home/ubuntu/${base}
+scp -i ${pem} ${1} ${userid}@${ec2_instance}:/home/${userid}/${base}
