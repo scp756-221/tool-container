7e0c762b4a78281dca56b91f9aa19710fd8f10e2
George Chow <george@overcoil.com>
2021-12-14 16:49:22 -0800

    add a 0-arg invocation option
    to use erun alias' output of an IP address

--- signon.sh
+++ signon.sh
@@ -3,25 +3,33 @@
 set -o errexit
 set -o nounset
 default_userid='ubuntu'
-if [[ $# -lt 1 || $# -gt 2 ]]
+if [[ $# -gt 3 ]]
 then
-  echo "Usage: signon.sh EC2-INSTANCE-NAME [EC2-USERID]"
-  echo " This command will save the instance name for later use by transfer.sh."
-  echo " EC2-INSTANCE-NAME should not include the userid for signing on."
-  echo " The instance userid is optional, with default '${default_userid}'."
+  echo "Usage: signon.sh [EC2-INSTANCE-NAME] [EC2-USERID]"
+  echo " This command signs onto your EC2 instance; all data is also saved for subsequent use by transfer.sh."
+  echo " You can either save the destination instance in advance (into ec2-instance-name.txt) or specify it here."
+  echo " If you specify the instance, you must also supply the userid. The default userid is '${default_userid}'."
   echo " Amazon Linux requires the userid 'ec2-user'."
   exit 1
 fi
 
-if [[ $# == 1 ]]
+if [[ $# == 0 ]]
 then
   userid=${default_userid}
-else
+fi
+
+if [[ $# == 1 ]]
+then
+  userid=${1}
+fi
+
+if [[ $# == 2 ]]
+then
+  echo ${1} > ec2-instance-name.txt
   userid=${2}
 fi
 
-echo ${1} > ec2-instance-name.txt
 echo ${userid} > ec2-userid.txt
 pem=$(cat ec2-pem-path.txt)
 set -o xtrace
-ssh -i ${pem} ${userid}@${1}
+ssh -i ${pem} ${userid}@`cat ec2-instance-name.txt`
